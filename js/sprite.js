99cf8d42a5324a7bec9324d71c03fe615c853861
lucas <lucaspenney@gmail.com>
2015-04-27 18:13:30 -0700

    Add some debugging stuff for firefox bug...

--- js/sprite.js
+++ js/sprite.js
@@ -23,6 +23,8 @@ var Sprite = Class.extend({
 			_this.yOffset = 0;
 			_this.width = _this.entity.width || _this.img.width;
 			_this.height = _this.entity.height || _this.img.height;
+			_this.imgWidth = _this.img.width;
+			_this.imgHeight = _this.img.height;
 			_this.frameWidth = _this.img.width;
 			_this.frameHeight = _this.img.height;
 			_this.rotationXOffset = 0;
@@ -39,7 +41,8 @@ var Sprite = Class.extend({
 			ctx.translate(x + this.rotationXOffset, y + this.rotationYOffset);
 			ctx.rotate(degToRad(this.entity.rotation));
 			ctx.globalAlpha = this.alpha;
-			ctx.drawImage(this.img, this.xOffset, this.yOffset, this.frameWidth, this.frameHeight, -this.width / 2, -this.height / 2, this.width, this.height);
+			if (this.frameWidth < 1 || this.frameHeight < 1 || this.frameWidth > this.imgWidth || this.frameHeight > this.imgHeight) console.log(this);
+			ctx.drawImage(this.img, this.xOffset, this.yOffset, this.frameWidth, this.frameHeight, -this.width / 2, -this.height / 2, this.imgWidth, this.imgHeight);
 			ctx.restore();
 		}
 	},
